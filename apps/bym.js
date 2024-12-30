dfb3b1b9fd83d028fe9fa16334178256c812c045
gaoao-3 <140887777+gaoao-3@users.noreply.github.com>
2024-12-30 23:10:11 +0800

    666

--- apps/bym.js
+++ apps/bym.js
@@ -106,9 +106,9 @@ export class bym extends plugin {
       let chats = await getChatHistoryGroup(e, 20)
       opt.system = `你的名字是“${Config.assistantLabel}”，你在一个qq群里，群号是${group},当前和你说话的人群名片是${card}, qq号是${sender}, 请你结合用户的发言和聊天记录作出回应，要求表现得随性一点，最好参与讨论，混入其中。与你相关的话题时，你可以去回应一下，不相关你就随便应付就行了，不要过分插科打诨，不知道说什么可以复读群友的话。当群友要求你做搜索、发图、发视频和音乐等操作时要使用工具，不可以直接发文字来蒙混过关，你必须根据需要来调用各种工具，并回复相关问题。要求优先使用中文进行对话。` +
         candidate +
-        '以下是聊天记录:’+ chats
-        .map(chat => {
-          const sender = chat.sender || chat || {}
+         '以下是聊天记录:' + chats
+          .map(chat => {
+            let sender = chat.sender || chat || {}
           return `【${sender.card || sender.nickname}】(qq：${sender.user_id}, ${roleMap[sender.role] || '未知角色'}, 群头衔：${sender.title || '无'}, 时间：${formatDate(new Date(chat.time * 1000))}, messageId: ${chat.message_id}) 说：${chat.raw_message}`
         })
           .join('\n') +
